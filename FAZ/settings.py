2d159365f0e6478c276a2b876eed2746859e8851
Dr Ahmed <abubakaramoka360@gmail.com>
2025-06-03 06:03:25 +0100

    Fix ALLOWED_HOSTS for Render deployment

--- FAZ/settings.py
+++ FAZ/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = 'django-insecure-9$t47%u=eg&gcos=2f6iqrxl9na_h3%jtk-taf*5j2%fm&y-at
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'fazcollegecompetitonwebsite.onrender.com']
 
 # CORS SETTINGS
 CORS_ALLOWED_ORIGINS = [
